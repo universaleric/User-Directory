c81a1721a12bdda12c9db88888235aedfa202bd7
Eric Hay <ericdanielhay@yahoo.com>
2021-05-14 14:21:13 -0500

    Updates

--- directory/src/utils/API.js
+++
@@ -1,9 +0,0 @@
-import axios from "axios";
-
-export default {
-  getRandomUser: function () {
-    return axios.get(
-      "https://randomuser.me/api/?inc=name,email,picture,cell&nat=us&results=100"
-    );
-  },
-};
